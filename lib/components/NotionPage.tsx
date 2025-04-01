85c851ea49247af84fc905cfd2b1c938cc3606a1
Bea M <64287361+beamariano@users.noreply.github.com>
2025-04-01 12:19:52 +0800

    hotfix: clean up

--- lib/components/NotionPage.tsx
+++ lib/components/NotionPage.tsx
@@ -1,51 +1,22 @@
 import React from 'react';
 
 import Head from 'next/head';
-import { defaultMapImageUrl, getPageImageUrls, getPageTitle } from 'notion-utils';
+import { getPageTitle } from 'notion-utils';
 import { NotionRenderer } from 'react-notion-x';
 import type { ExtendedRecordMap } from 'notion-types';
 
 export function NotionPage({
 	recordMap,
-	rootPageId,
+	// rootPageId,
 }: {
 	recordMap: ExtendedRecordMap;
-	rootPageId?: string;
+	// rootPageId?: string;
 }) {
 	if (!recordMap) {
 		return null;
 	}
 
 	const title = getPageTitle(recordMap);
-	// const image = getPageImageUrls(recordMap);
-
-	// console.log(image);
-
-	// console.log(title);
-	console.log(recordMap, '\n\n');
-
-	const urls: string[] = getPageImageUrls(recordMap, {
-		mapImageUrl: defaultMapImageUrl
-	})
-
-	console.log(urls[0]);
-
-	// const previewImagesMap = Object.fromEntries(
-	// 	await pMap(urls, async (url) => [url, await getPreviewImage(url)], {
-	// 		concurrency: 8
-	// 	})
-	// )
-
-	// return previewImagesMap
-	// }
-
-	// const header = () => {
-	// 	<div>
-	// 		<Image>
-
-	// 		</Image>
-	// 	</div>
-	// }
 
 	return (
 		<>
@@ -57,22 +28,9 @@ export function NotionPage({
 				<title>{title}</title>
 			</Head>
 			<NotionRenderer
-
 				recordMap={recordMap}
-			// components={{ Image: urls }}
-			// darkMode={true}
 			/>
-			{/* <NotionRenderer image */}
-			{/* <NotionRenderer
-				// showTableOfContents={true}
-				// components={}
-				// forceCustomImages
-				recordMap={recordMap}
-				fullPage={true}
-				darkMode={false}
-				rootPageId={rootPageId}
-				disableHeader={true}
-			/> */}
+
 		</>
 	);
 }
