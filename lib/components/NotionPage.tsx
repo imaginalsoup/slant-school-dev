c99fe838095b4c134b9472b2a49cb785d293fac9
Bea M <64287361+beamariano@users.noreply.github.com>
2025-04-01 15:07:04 +0800

    feat: add banner

--- lib/components/NotionPage.tsx
+++ lib/components/NotionPage.tsx
@@ -1,78 +1,77 @@
-import React from 'react';
+// import React from 'react';
 
-import Head from 'next/head';
-import { defaultMapImageUrl, getPageImageUrls, getPageTitle } from 'notion-utils';
-import { NotionRenderer } from 'react-notion-x';
-import type { ExtendedRecordMap } from 'notion-types';
+// import Head from 'next/head';
+// import { defaultMapImageUrl, getPageImageUrls, getPageTitle } from 'notion-utils';
+// import { NotionRenderer } from 'react-notion-x';
+// import type { ExtendedRecordMap } from 'notion-types';
 
-export function NotionPage({
-	recordMap,
-	rootPageId,
-}: {
-	recordMap: ExtendedRecordMap;
-	rootPageId?: string;
-}) {
-	if (!recordMap) {
-		return null;
-	}
+// export function NotionPage({
+// 	recordMap,
+// 	rootPageId,
+// }: {
+// 	recordMap: ExtendedRecordMap;
+// 	rootPageId?: string;
+// }) {
+// 	if (!recordMap) {
+// 		return null;
+// 	}
 
-	const title = getPageTitle(recordMap);
-	// const image = getPageImageUrls(recordMap);
+// 	const title = getPageTitle(recordMap);
+// 	// const image = getPageImageUrls(recordMap);
 
-	// console.log(image);
+// 	// console.log(image);
 
-	// console.log(title);
-	console.log(recordMap, '\n\n');
+// 	// console.log(title);
+// 	console.log(recordMap, '\n\n');
 
-	const urls: string[] = getPageImageUrls(recordMap, {
-		mapImageUrl: defaultMapImageUrl
-	})
+// 	const bannerImage: string = getPageImageUrls(recordMap, {
+// 		mapImageUrl: defaultMapImageUrl
+// 	})[0];
 
-	console.log(urls[0]);
+// 	console.log(bannerImage);
 
-	// const previewImagesMap = Object.fromEntries(
-	// 	await pMap(urls, async (url) => [url, await getPreviewImage(url)], {
-	// 		concurrency: 8
-	// 	})
-	// )
+// 	// const previewImagesMap = Object.fromEntries(
+// 	// 	await pMap(urls, async (url) => [url, await getPreviewImage(url)], {
+// 	// 		concurrency: 8
+// 	// 	})
+// 	// )
 
-	// return previewImagesMap
-	// }
+// 	// return previewImagesMap
+// 	// }
 
-	// const header = () => {
-	// 	<div>
-	// 		<Image>
+// 	// const header = () => {
+// 	// 	<div>
+// 	// 		<Image>
 
-	// 		</Image>
-	// 	</div>
-	// }
+// 	// 		</Image>
+// 	// 	</div>
+// 	// }
 
-	return (
-		<>
-			<Head>
-				<meta
-					name="description"
-					content=""
-				/>
-				<title>{title}</title>
-			</Head>
-			<NotionRenderer
-
-				recordMap={recordMap}
-			// components={{ Image: urls }}
-			// darkMode={true}
-			/>
-			{/* <NotionRenderer image */}
-			{/* <NotionRenderer
-				// showTableOfContents={true}
-				// components={}
-				// forceCustomImages
-				recordMap={recordMap}
-				fullPage={true}
-				darkMode={false}
-				rootPageId={rootPageId}
-				disableHeader={true}
-			/> */}
-		</>
-	);
-}
+// 	return (
+// 		<>
+// 			<Head>
+// 				<meta
+// 					name="description"
+// 					content=""
+// 				/>
+// 				<title>{title}</title>
+// 			</Head>
+// 			<NotionRenderer
+// 				recordMap={recordMap}
+// 			// components={{ Image: urls }}
+// 			// darkMode={true}
+// 			/>
+// 			{/* <NotionRenderer image */}
+// 			{/* <NotionRenderer
+// 				// showTableOfContents={true}
+// 				// components={}
+// 				// forceCustomImages
+// 				recordMap={recordMap}
+// 				fullPage={true}
+// 				darkMode={false}
+// 				rootPageId={rootPageId}
+// 				disableHeader={true}
+// 			/> */}
+// 		</>
+// 	);
+// }
--- lib/modules/home/HomeScreen.tsx
+++ lib/modules/home/HomeScreen.tsx
@@ -1,26 +1,38 @@
 import React from 'react'
 import type { ExtendedRecordMap } from 'notion-types';
 import { NotionRenderer } from 'react-notion-x';
-import { getPageImageUrls, defaultMapImageUrl, getTextContent } from 'notion-utils';
-import { Code } from 'react-notion-x/build/third-party/code';
+import { getPageImageUrls, defaultMapImageUrl, getPageTitle, getBlockTitle } from 'notion-utils';
 
 type Props = { recordMap: ExtendedRecordMap }
 
 
 const HomeScreen = ({ recordMap }: Props) => {
-
-
 	const bannerImage: string = getPageImageUrls(recordMap, {
 		mapImageUrl: defaultMapImageUrl
 	})[0];
 
+	const title = getPageTitle(recordMap);
+
 	return (
-		<div className='banner notion-page-cover-wrapper'>
-			<img src={bannerImage} alt='banner' />
-			<NotionRenderer
-				recordMap={recordMap}
-			/>
-		</div >
+		<div>
+			<div className='banner notion-page-cover-wrapper'
+				style={{
+					backgroundImage: `linear-gradient(to bottom, #2E210BEF, rgba(245, 246, 252, 0.02),
+				#2E210BE1), url(${bannerImage})`,
+					backgroundPosition: 'center',
+					backgroundSize: 'cover',
+					// backgroundRepeat: 'no-repeat'
+				}}
+			>
+				<div className='hero-text'>
+					<h1>{title}</h1>
+					<NotionRenderer
+						disableHeader={true}
+						recordMap={recordMap}
+					/>
+				</div>
+			</div >
+		</div>
 	);
 }
 
