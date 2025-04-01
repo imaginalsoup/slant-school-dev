9b93803799721cefb434146fc2952242a9eec753
Bea M <64287361+beamariano@users.noreply.github.com>
2025-04-01 16:16:50 +0800

    hotfix: return authtoken and user

--- lib/config.ts
+++ lib/config.ts
@@ -1,9 +1,5 @@
 // ENV config
 export const rootNotionPageId = process.env.ROOT_PAGE_ID;
 export const home = process.env.ROOT_PAGE_ID;
-
-
-// export const authToken = process.env.TOKEN;
-// export const user = process.env.USER_ID;
-
-// export const HOME_BANNER = 
+export const authToken = process.env.TOKEN;
+export const user = process.env.USER_ID;
