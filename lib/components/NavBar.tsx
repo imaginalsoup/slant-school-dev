80e6da76723d83064f36bc8b9e788826242ea1aa
Bea M <64287361+beamariano@users.noreply.github.com>
2025-04-03 10:54:18 +0800

    remove: lectures

--- lib/components/NavBar.tsx
+++ lib/components/NavBar.tsx
@@ -1,20 +1,20 @@
 import { HStack } from '@chakra-ui/react'
 import React from 'react'
 import Logo from './Logo'
-import Link from 'next/link'
+// import Link from 'next/link'
 
 const NavBar = () => {
 	return (
-		<HStack height='48px' justifyContent='space-between' padding='24px' background='transparent' className='navbar'>
+		<HStack height='48px' justifyContent='space-between' padding='24px' background='transparent' className='navbar' paddingBottom='12px'>
 			<Logo />
-			<HStack gap={12} >
+			{/* <HStack gap={12} >
 				<Link href='/lectures'>
 					lectures
 				</Link>
 				<Link href='/about'>
 					about
 				</Link>
-			</HStack>
+			</HStack> */}
 		</HStack>)
 }
 
